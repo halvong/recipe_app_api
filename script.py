93efb6a6444cf0dad4fff3ba2d2e6965d6b060d8
Hal Vong <halvong@yahoo.com>
2019-03-12 09:27:57 -0700

    delete script.py

--- script.py
+++
@@ -1,6 +0,0 @@
-#from django.shortcuts import render, get_object_or_404
-#from django.contrib.auth.models import User
-#from django.db.models import Count
-#from django.core.mail import send_mail
-#from blogi.models import Post
-#from taggit.models import Tag
